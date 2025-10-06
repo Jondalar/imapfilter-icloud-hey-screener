0814e0e70e7eb2da769d87b8242eb68bf6ec3c30
Alex Damhuis <alex.damhuis@icloud.com>
2025-10-06 12:45:22 +0200

    changed to use JUNK folder, also added cleanup to other folders by moving to archive after 30 days

--- imap-screener/imapfilter/config.lua
+++ imap-screener/imapfilter/config.lua
@@ -1,17 +1,19 @@
 --------------------------------------------------------------------
--- imapfilter Screener – Quelle: SCREENED (iCloud-Serverregel)
+-- imapfilter Screener – iCloud-Serverregel + INBOX Catch-up
 -- Fluss:
 --   0) Warmup: alle MIDs in Screened indizieren (Approve funktioniert sofort)
---   1) Sortieren AUS Screened:
+--   0a) CATCH-UP: Alles, was doch in INBOX landet (z. B. Hide-My-Mail),
+--       wird nach Screened verschoben (Blocklist raus, Whitelist bleibt).
+--   1) AUS SCREENED SORTIEREN:
 --        Blocklist  -> Blackhole
 --        Whitelist  -> INBOX
---        Receipts   -> Receipts (Trainingsliste + Subject-Heuristik)
---        Newsletters-> Newsletters (Trainingsliste + List-Header)
+--        Receipts   -> Receipts (Training + Heuristik)
+--        Newsletters-> Newsletters (Training + List-Header)
 --        Rest bleibt in Screened
---   2) Training: manuell verschobene Mails trainieren Listen (additiv)
---   3) Approve: Mail von Screened -> INBOX (vom Nutzer) => Absender whitelisten
+--   2) TRAINING: manuell verschobene Mails -> Listen (additiv)
+--   3) APPROVE: Screened -> INBOX (vom Nutzer) => Whitelist
 -- Technisch:
---   - Nur contain_* (keine Regex) für IMAP-Search
+--   - Nur contain_* (keine Regex)
 --   - Set-basierte Moves
 --   - Ordner-Autocreate/-Subscribe
 --   - Header-Handling nil-sicher, Message-ID robust
@@ -55,6 +57,14 @@ local function file_set(path)
   return t
 end
 
+-- Listen laden
+local whitelist      = file_set(wl_path)
+local blocklist      = file_set(bl_path)
+local screened_ids   = file_set(sid_path)
+local receiptlist    = file_set(rcl_path)
+local newsletterlist = file_set(nsl_path)
+
+
 local function file_add_line(path, line)
   local f = io.open(path, 'a'); f:write(line.."\n"); f:close()
 end
@@ -85,6 +95,26 @@ local function parse_from(addr)
   return m:lower():gsub("^%s+",""):gsub("%s+$","")
 end
 
+-- Extrahiere eine nutzbare Absenderadresse (From -> Sender -> Return-Path)
+local function extract_sender(hdr)
+  local function pick(addr)
+    if not addr or addr == "" then return nil end
+    -- falls mehrere Adressen: nimm die erste
+    addr = addr:match("<([^>]+)>") or addr
+    addr = addr:gsub("^%s+",""):gsub("%s+$","")
+    -- sehr defensive Normalisierung
+    addr = addr:lower()
+    -- ganz grob validieren
+    if addr:find("@") then return addr end
+    return nil
+  end
+
+  local from = header_field(hdr, "From")
+  local sender = header_field(hdr, "Sender")
+  local rpath = header_field(hdr, "Return%-Path")
+  return pick(from) or pick(sender) or pick(rpath)
+end
+
 -- Message-ID robust ziehen (alle Schreibweisen) + <...> entfernen
 local function get_message_id(hdr)
   if not hdr or hdr == "" then return nil end
@@ -99,12 +129,14 @@ local function get_message_id(hdr)
   return nil
 end
 
--- Listen laden
-local whitelist      = file_set(wl_path)
-local blocklist      = file_set(bl_path)
-local screened_ids   = file_set(sid_path)
-local receiptlist    = file_set(rcl_path)
-local newsletterlist = file_set(nsl_path)
+-- -- Remove MID from screened_ids.{mem+file}, if present
+local function clear_sid_for_msg(mbox, uid)
+  local mid = get_message_id(safe_header(mbox, uid))
+  if mid and screened_ids[mid:lower()] then
+    file_del_line(sid_path, mid:lower())
+    screened_ids[mid:lower()] = nil
+  end
+end
 
 -- Wildcards *@domain.tld
 local function is_wildcard(entry) return entry:sub(1,2) == '*@' end
@@ -126,9 +158,13 @@ end
 
 local INBOX       = account['INBOX']
 local SCREENED    = ensure_box('Screened')
-local BLACKHOLE   = ensure_box('Blackhole')
 local RECEIPTS    = ensure_box('Receipts')
 local NEWSLETTERS = ensure_box('Newsletters')
+
+-- Standard system folders on iCloud
+local JUNK          = ensure_box('Junk')
+local DELETED       = ensure_box('Deleted Messages')
+local ARCHIVE       = ensure_box('Archive')
 
 --------------------------------------------------------------------
 -- 0) WARMUP: alle MIDs in Screened indizieren (für Approve)
@@ -145,29 +181,80 @@ do
 end
 
 --------------------------------------------------------------------
+-- 0a) INBOX CATCH-UP (Variante C):
+--     Alles, was dennoch in INBOX landet (z. B. Hide-My-Mail),
+--     wird nachträglich nach Screened geräumt:
+--     - Blocklist -> Blackhole
+--     - Whitelist bleibt in INBOX
+--     - Rest -> Screened (+MID loggen)
+--------------------------------------------------------------------
+do
+  local unseen = INBOX:is_unseen()
+
+  -- Blocklist -> Blackhole
+  local to_bh = Set {}
+  for sender,_ in pairs(blocklist) do
+    if is_wildcard(sender) then
+      local dom = wildcard_domain(sender)
+      to_bh = to_bh + unseen:contain_from('@'..dom) + unseen:contain_from('.'..dom)
+    else
+      to_bh = to_bh + unseen:contain_from(sender)
+    end
+  end
+
+  -- Rest ohne Whitelist
+  local rest = INBOX:is_unseen()
+  local wlset = Set {}
+  for sender,_ in pairs(whitelist) do
+    if is_wildcard(sender) then
+      local dom = wildcard_domain(sender)
+      wlset = wlset + rest:contain_from('@'..dom) + rest:contain_from('.'..dom)
+    else
+      wlset = wlset + rest:contain_from(sender)
+    end
+  end
+  rest = rest - wlset
+
+  -- MIDs loggen, dann Rest -> Screened
+  for _, msg in ipairs(rest:select_all()) do
+    local mbox, uid = table.unpack(msg)
+    local mid = get_message_id(safe_header(mbox, uid))
+    if mid and not screened_ids[mid:lower()] then
+      file_add_line(sid_path, mid:lower()); screened_ids[mid:lower()] = true
+    end
+  end
+  rest:move_messages(SCREENED)
+end
+
+--------------------------------------------------------------------
 -- 1) AUS SCREENED SORTIEREN
---    Blocklist -> Blackhole, Whitelist -> INBOX,
---    dann Receipts, dann Newsletters; Rest bleibt in Screened
 --------------------------------------------------------------------
 do
   local src = SCREENED:select_all()
 
-  -- Blocklist
-  do
-    local to_bh = Set {}
-    for sender,_ in pairs(blocklist) do
-      if is_wildcard(sender) then
-        local dom = wildcard_domain(sender)
-        to_bh = to_bh + src:contain_from('@'..dom) + src:contain_from('.'..dom)
-      else
-        to_bh = to_bh + src:contain_from(sender)
+--- Junk -> blocklist (Resync)
+do
+  for _, msg in ipairs(JUNK:select_all()) do
+    local mbox, uid = table.unpack(msg)
+    local hdr  = safe_header(mbox, uid)
+    local addr = extract_sender(hdr)
+    if addr and not blocklist[addr] then
+      file_add_line(bl_path, addr); blocklist[addr] = true
+      -- optional: auch Domain sperren
+      if ADD_DOMAIN_ON_JUNK then
+        local dom = email_domain(addr)
+        if dom and not blocklist["*@"..dom] then
+          file_add_line(bl_path, "*@"..dom); blocklist["*@"..dom] = true
+        end
       end
+      io.stdout:write(string.format("[train] added to blocklist: %s\n", addr))
     end
-    to_bh:move_messages(BLACKHOLE)
-    src = SCREENED:select_all()  -- Quelle aktualisieren
+    -- MID aus screened_ids.txt löschen, falls noch dort
+    clear_sid_for_msg(mbox, uid)
   end
+end
 
-  -- Whitelist
+  -- Whitelist -> INBOX
   do
     local to_inbox = Set {}
     for sender,_ in pairs(whitelist) do
@@ -182,7 +269,7 @@ do
     src = SCREENED:select_all()
   end
 
-  -- Receipts (trainierte Absender + Subject-Heuristik)
+  -- Receipts (Training + Subject-Heuristik)
   do
     local rset = Set {}
     for sender,_ in pairs(receiptlist) do
@@ -200,7 +287,7 @@ do
     src = SCREENED:select_all()
   end
 
-  -- Newsletters (trainierte Absender + List-Header)
+  -- Newsletters (Training + List-Header)
   do
     local nlset = Set {}
     for sender,_ in pairs(newsletterlist) do
@@ -216,32 +303,55 @@ do
     nlset = nlset + src:contain_field('List-Unsubscribe', 'mailto:')
     nlset:move_messages(NEWSLETTERS)
     -- Rest bleibt in Screened
+
+    -- NEU: alles Ungelesene in "Newsletters" als gelesen markieren
+  NEWSLETTERS:is_unseen():mark_seen()
   end
 end
 
 --------------------------------------------------------------------
 -- 2) TRAINING: manuell verschobene Mails -> Listen (additiv)
+--     Junk ersetzt Blackhole vollständig
 --------------------------------------------------------------------
 do
-  for _, msg in ipairs(BLACKHOLE:select_all()) do
+  -- Junk -> blocklist (resync)
+  for _, msg in ipairs(JUNK:select_all()) do
     local mbox, uid = table.unpack(msg)
-    local from = parse_from(header_field(safe_header(mbox, uid), "From"))
-    if from and not blocklist[from] then file_add_line(bl_path, from); blocklist[from]=true end
+    local hdr  = safe_header(mbox, uid)
+    local addr = extract_sender and extract_sender(hdr) or parse_from(header_field(hdr, "From"))
+    if addr and not blocklist[addr] then
+      file_add_line(bl_path, addr)
+      blocklist[addr] = true
+      io.stdout:write(string.format("[train] added to blocklist: %s\n", addr))
+    end
+    if clear_sid_for_msg then clear_sid_for_msg(mbox, uid) end
   end
+
+  -- Receipts -> receiptlist
   for _, msg in ipairs(RECEIPTS:select_all()) do
     local mbox, uid = table.unpack(msg)
     local from = parse_from(header_field(safe_header(mbox, uid), "From"))
-    if from and not receiptlist[from] then file_add_line(rcl_path, from); receiptlist[from]=true end
+    if from and not receiptlist[from] then
+      file_add_line(rcl_path, from)
+      receiptlist[from] = true
+    end
+    clear_sid_for_msg(mbox, uid)
   end
+
+  -- Newsletters -> newsletterlist
   for _, msg in ipairs(NEWSLETTERS:select_all()) do
     local mbox, uid = table.unpack(msg)
     local from = parse_from(header_field(safe_header(mbox, uid), "From"))
-    if from and not newsletterlist[from] then file_add_line(nsl_path, from); newsletterlist[from]=true end
+    if from and not newsletterlist[from] then
+      file_add_line(nsl_path, from)
+      newsletterlist[from] = true
+    end
+    clear_sid_for_msg(mbox, uid)
   end
 end
 
 --------------------------------------------------------------------
--- 3) APPROVE: Screened -> INBOX (vom Nutzer) => Absender whitelisten
+-- 3) APPROVE: Screened -> INBOX (vom Nutzer) => Whitelist
 --------------------------------------------------------------------
 do
   for _, msg in ipairs(INBOX:select_all()) do
@@ -257,6 +367,23 @@ do
       -- io.stdout:write("[approve] MID="..mid.." FROM="..(from or "nil").."\n")
     end
   end
+end
+
+
+--------------------------------------------------------------------
+-- Maintenance
+-- 1) Junk: mark all unread as seen; older than 7 days -> Deleted Messages
+-- 2) Receipts: older than 30 days -> Archive
+--------------------------------------------------------------------
+do
+  -- Junk aufräumen
+  JUNK:is_unseen():mark_seen()
+  local junk_old = JUNK:is_older(7)
+  junk_old:move_messages(DELETED)
+
+  -- Receipts archivieren
+  local rc_old = RECEIPTS:is_older(30)
+  rc_old:move_messages(ARCHIVE)
 end
 
 -- Ende
